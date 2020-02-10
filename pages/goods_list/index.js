3cb28af490b6e037a150582953e4a8f584c5ba27
Macro <macro224@163.com>
2020-02-11 01:50:37 +0800

    5-0 优化商品列表功能，下拉刷新、上拉加载下一页，修改商品分类页面点击跳转传的参数，创建了商品详情页面但还没开始写

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,16 +1,22 @@
 // pages/goods_list/index.js
 import { request } from '../../requests/request.js'
 Page({
-
+  QueryParams: {
+    cid: '',
+    query: '',
+    pagenum: 1,
+    pagesize: 10
+  },
+  totalPages: 0,
   /**
    * 页面的初始数据
    */
   data: {
-    tabs:[
+    tabs: [
       {
         id: 0,
-        value:'综合',
-        isActive:true
+        value: '综合',
+        isActive: true
       }, {
         id: 1,
         value: '销量',
@@ -24,19 +30,32 @@ Page({
     goodsList: []  //商品列表
   },
   // 点击子组件修改父组件中的tabs数据的函数
-  tabsChange:function(e){
+  tabsChange: function (e) {
     this.setData({
       tabs: e.detail
     })
   },
+  // 获取商品列表数据
+  getGoodsList:async function(){
+    const goodsList = await request({ url: '/goods/search', data: this.QueryParams });
+    // 获取总条数
+    const total = goodsList.total;
+    // 计算总条数
+    this.totalPages=Math.ceil(total/this.QueryParams.pagesize)
+    // 赋值用于页面渲染
+    this.setData({
+      goodsList: [...this.data.goodsList, ...goodsList.goods]
+    })
+    // 关闭下拉刷新
+    wx.stopPullDownRefresh()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: async function (options) {
-    const goodsList = await request({ url: '/goods/search', data: { query: options.query} })
-    this.setData({
-      goodsList
-    })
+  onLoad: function (options) {
+    this.QueryParams.cid = options.cid
+    this.QueryParams.query = options.query
+    this.getGoodsList()
   },
 
   /**
@@ -71,14 +90,29 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.QueryParams.pagenum = 1;
+    this.setData({
+      goodsList: []
+    })
+    this.getGoodsList()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    // 判断是否有下一页数据
+    if(this.QueryParams.pagenum>=this.totalPages){
+      // 没有 给出提示
+      wx.showToast({
+        title: '没有下一页数据...',
+        icon: 'none'
+      })
+    }else{
+      // 有 页数+1并重新获取商品列表数据
+      this.QueryParams.pagenum++;
+      this.getGoodsList()
+    }
   },
 
   /**
--- requests/url.js
+++ requests/url.js
@@ -1,1 +1,2 @@
 export const BASE_URL = "https://www.linweiqin.cn/api/public/v1"
+// export const BASE_URL = "https://api.zbztb.cn/api/public/v1"
