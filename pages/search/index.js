e9736cdea65e9c1afcf23ba270ed9c9a3514250a
Macro <macro224@163.com>
2020-02-10 17:10:12 +0800

    3-0 商品搜索页面的渲染完成

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,18 +1,39 @@
 // pages/search/index.js
+import { request } from '../../requests/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodList:'', //商品数据
+    sosuoVal:'' //输入内容
   },
-
+  // 获取商品数据
+  getGoods:async function(){
+    if (this.data.sosuoVal){
+      const goodList = await request({ url: '/goods/qsearch', data: { query: this.data.sosuoVal.trim()} })
+      this.setData({
+        goodList
+      })
+    }else{
+      wx.showToast({
+        title: '请重新输入...',
+        icon: 'none',
+        mask: true
+      })
+    }
+  },
+  bandInput:function(e){
+    this.setData({
+      sosuoVal: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
