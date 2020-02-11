d28428b64c7ccf97c7f35756c49c616e3509481a
Macro <macro224@163.com>
2020-02-11 17:43:04 +0800

    6-0 完成商品详情页面渲染和功能，收藏、加入购物车、轮播图、页面跳转

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,19 +6,93 @@ Page({
    * 页面的初始数据
    */
   data: {
-    good:'' //商品数据
+    good:'', //商品数据
+    isCollect: false  //是否被收藏
   },
-
+  goods_id: '',
+  // 获取商品数据函数
+  async getGood(){
+    const good = await request({ url: '/goods/detail', data: { goods_id: this.goods_id } })
+    // 根据本地数据判断该商品已经被收藏
+    let collect = wx.getStorageSync("collect") || [];
+    let isCollect = collect.some(v => v.goods_id === good.goods_id)
+    this.setData({
+      good, isCollect
+    })
+  },
+  // 点击图片预览大图函数
+  handMaximg(e){
+    const urls=this.data.good.pics.map(v=>v.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current,
+      urls,
+    })
+  },
+  // 点击收藏和取消收藏
+  handShoucang(){
+    let isCollect = false;
+    // 将商品详情信息存到本地 collect
+    let collect = wx.getStorageSync("collect")||[];
+    // 判断点击的商品在本地是否已经存在 返回-1则不存在
+    let index = collect.findIndex(v=>v.goods_id===this.data.good.goods_id)
+    if(index!=-1){
+      // 已存在，收藏
+      isCollect = false;
+      collect.splice(index,1);
+      // 取消完成后给出提示
+      wx.showToast({
+        title: '取消成功',
+        icon: 'success',
+        mask: true
+      })
+    }else{
+      // 不存在，未收藏
+      isCollect = true;
+      collect.push({ goods_id: this.data.good.goods_id});
+      // 收藏完成后给出提示
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        mask: true
+      })
+    }
+    wx.setStorageSync("collect", collect)
+    // 将计算后的isCollect赋值回去
+    this.setData({
+      isCollect
+    })
+  },
+  // 点击加入购物车
+  handCartadd(){
+    let carts = wx.getStorageSync("carts") || [];
+    let index = carts.findIndex(v=>v.goods_id===this.data.good.goods_id)
+    // 判断index是否为-1，是则不存在===第一次添加购物车
+    if(index!=-1){
+      // 已存在购物车
+      carts[index].num++;
+    }else{
+      let good = this.data.good
+      // 不存在购物车，第一次加入购物车
+      good.num = 1; //商品数量
+      good.checked = true;
+      carts.push( good )
+    }
+    wx.setStorageSync("carts", carts);
+    // 加入完成后给出提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: async function (options) {
-    const good = await request({ url: '/goods/detail', data: { goods_id: options.goods_id } })
-    this.setData({
-      good
-    })
+  onLoad: function (options) {
+    this.goods_id = options.goods_id
+    this.getGood()
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
